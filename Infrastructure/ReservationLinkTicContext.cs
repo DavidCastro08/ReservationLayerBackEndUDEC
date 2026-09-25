7772f6e15fe83b0ec2e0d42db17462c0985d8bf4
DavidCastro08 <71454793+DavidCastro08@users.noreply.github.com>
2026-09-25 12:08:48 -0500

    secretes

--- Infrastructure/ReservationLinkTicContext.cs
+++ Infrastructure/ReservationLinkTicContext.cs
@@ -26,7 +26,7 @@ namespace ReservationArquitectureLayerUDEC.Infrastructure
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-BFQK93ER;Database= ReservationArquitectureLayerUDEC;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=privado;Database= privado;Trusted_Connection=True;");
             }
         }
 
